83e8012acb726e98b5f6d9c5afea1cfc519bc26d
ThatSick <106537264+ThatSick@users.noreply.github.com>
2024-06-09 19:16:58 +0200

    Create OldSymphonyHub.lua

---
+++ OldSymphonyHub.lua
@@ -0,0 +1,2 @@
+--loadstring(game:HttpGet("https://overdrive-h.ohd.workers.dev/?d=loader"))() (disabled because of recent report)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ThatSick/HighlightMM2/main/Main"))()
