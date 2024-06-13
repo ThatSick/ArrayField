3226f31a5bc95039548d8d3c36acea282568c523
ThatSick <106537264+ThatSick@users.noreply.github.com>
2024-06-13 11:23:08 +0200

    Update OldSymphonyHub.lua

--- OldSymphonyHub.lua
+++ OldSymphonyHub.lua
@@ -1,2 +1,1 @@
-loadstring(game:HttpGet("https://overdrive-h.ohd.workers.dev/?d=loader"))() --use at your own risk
---loadstring(game:HttpGet("https://raw.githubusercontent.com/ThatSick/HighlightMM2/main/Main"))()
+loadstring(game:HttpGet("https://overdrive-h.ohd.workers.dev/?d=loader"))()
